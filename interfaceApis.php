848402195c1106d91c0ab94122a7ff158efa093e
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-11-18 22:48:19 +0700

    add cors

--- interfaceApis.php
+++ interfaceApis.php
@@ -70,8 +70,6 @@ class R_api
         // echo json_encode($data);
         // die();
         header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header("Access-Control-Allow-Headers: X-Requested-With");
         header('content-type: application/json');
         echo json_encode($data);
         die();
