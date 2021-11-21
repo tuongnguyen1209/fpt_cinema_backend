b71ad5310c3fe02aa9deda1baf43aa9c2ffdc747
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-11-21 17:39:57 +0700

    fix production

--- controllers/room/show.php
+++ controllers/room/show.php
@@ -1,40 +1,37 @@
 <?php
-    $db = new db();
-    $connect = $db->connect();
+$db = new db();
+$connect = $db->connect();
 
-    $room = new room($connect);
-    $room->id_room = isset($_GET['id_room']) ;
-    if($room->id_room){
-        $room->id_room =$_GET['id_room'];
-        $room->show();
+$room = new room($connect);
+$room->id_room = isset($_GET['id_room']);
+if ($room->id_room) {
+    $room->id_room = $_GET['id_room'];
+    $room->show();
 
-        $room_item = array(
-            'id_room' => $room->id_room,
-            'tên phòng' => $room->name
-        );
-    
-        print_r(json_encode($room_item));
-    }else{
-        $read = $room->read();
-    
-        $num = $read->rowCount();
-        if($num>0){
-            $room_array = [];
-            $room_array['room']=[];
+    $room_item = array(
+        'id_room' => $room->id_room,
+        'name_room' => $room->name
+    );
 
-            while($row = $read->fetch(PDO::FETCH_ASSOC)){
-                extract($row);
+    print_r(json_encode($room_item));
+} else {
+    $read = $room->read();
 
-                $room_item = array(
-                    'id_room' => $id_room,
-                    'tên phòng' => $name
-                );
+    $num = $read->rowCount();
+    if ($num > 0) {
+        $room_array = [];
+        $room_array['room'] = [];
 
-                array_push($room_array['room'],$room_item);
-            }
-            echo json_encode($room_array);
+        while ($row = $read->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $room_item = array(
+                'id_room' => $id_room,
+                'name_room' => $name
+            );
+
+            array_push($room_array['room'], $room_item);
         }
+        echo json_encode($room_array);
     }
-
-    
-?>
+}
