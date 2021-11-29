f157879eb7d26e19a1e15eef8bfb127cb6978f22
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-11-29 16:45:04 +0700

    fix create

--- controllers/user/create.php
+++ controllers/user/create.php
@@ -4,19 +4,6 @@ $connect = $db->connect();
 
 $user = new user($connect);
 
-<<<<<<< HEAD
-    $data = json_decode(file_get_contents("php://input"));
-    $user->full_name = $data->full_name;
-    $user->email = $data->email;
-    $user->phone = $data->phone;
-    $user->password = $data->password;
-    $user->status = $data->status;
-    if($user->create()){
-        echo json_encode(array('message','Qestion Created'));
-    }else{
-        echo json_encode(array('message','Qestion Not Created'));
-    }
-=======
 $data = json_decode(file_get_contents("php://input"));
 $user->full_name = $data->full_name;
 $user->email = $data->email;
@@ -29,4 +16,3 @@ if ($user->create()) {
 } else {
     $this->response(200, array('message', 'Qestion Not Created'));
 }
->>>>>>> e92aa350a9dffabfaf8b8f2e732a4e6f2e09eb8c
