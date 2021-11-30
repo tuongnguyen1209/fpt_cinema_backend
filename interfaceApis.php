272b793a0d2f520da91a8a26c22918e623d6ef36
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-11-30 23:56:00 +0700

    fix movie

--- interfaceApis.php
+++ interfaceApis.php
@@ -71,7 +71,7 @@ class R_api
         // die();
         header('Access-Control-Allow-Origin: *');
 
-        header('Access-Control-Allow-Methods: GET, POST');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
         header("Access-Control-Allow-Headers: *");
 
