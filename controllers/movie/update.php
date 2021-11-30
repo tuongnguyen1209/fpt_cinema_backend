fd0204633f755e87f569236d275aac78f60f68c6
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-11-30 23:35:10 +0700

    fix movie

--- controllers/movie/update.php
+++ controllers/movie/update.php
@@ -8,7 +8,8 @@ $data = json_decode(file_get_contents("php://input"));
 $movie->id_movie = isset($_GET['id_movie']) ? $_GET['id_movie'] : die();
 
 $movie->name_mv = $data->name_mv;
-$movie->image_mv = $data->image_mv;
+$movie->image_lage = $data->image_lage;
+$movie->image_medium = $data->image_medium;
 $movie->traller = $data->traller;
 $movie->date_start = $data->date_start;
 $movie->date_end = $data->date_end;
@@ -20,14 +21,14 @@ $movie->time_mv = $data->time_mv;
 
 $movie->banner  = $data->banner;
 $movie->rate  = $data->rate;
-$movie->status = $data->status;
 $movie->country = $data->country;
 $movie->production = $data->production;
 $movie->name_vn = $data->name_vn;
+$movie->status = 1;
 
 // $movie->update();
 $ar = $data->id_cate;
-if ($movie->update()) {
+ if ($movie->update()) {
     $movie->delete_CT_MV();
     for ($i = 0; $i < count($ar); $i++) {
         $movie->id_cate = $data->id_cate = $ar[$i];
@@ -37,7 +38,7 @@ if ($movie->update()) {
         'status' => 'success',
         'data' => $movie,
     );
-    $this->response(200, $response);
+     $this->response(200, $response);
 } else {
     $this->response(401, array(
         'status' => 'False',
