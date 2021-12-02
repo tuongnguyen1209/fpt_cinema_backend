7214a5f24b21fab6634ad22614aaaba3e1ed389d
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-12-03 00:38:22 +0700

    login facebook

--- controllers/auth/login.php
+++ controllers/auth/login.php
@@ -9,66 +9,68 @@ $user = new user($connect);
 
 
 $type = isset($_GET['type']) ? $_GET['type'] : null;
-
-if ($type === 'fb') {
-    $facebookId = $this->params->id;
-    if ($user->checkFacebook($facebookId)) {
-        $response = array(
-            'status' => 'success',
-            'data' => $user,
-        );
-        $this->response(200, $response);
-    } else {
-        $data = json_decode(file_get_contents("php://input"));
-        $user->full_name = isset($data->name) ? $data->name : '';
-        $user->email = isset($data->email) ? $data->email : '';
-        $user->phone = isset($data->phone) ? $data->phone : '';
-        $user->password = md5($facebookId);
-        $user->status = 0;
-        $user->administration = 0;
-        $user->img_user = isset($data->picture) ? $data->picture->data->url : '';
-        if ($user->create(null, $facebookId)) {
+if (isset($type)) {
+    if ($type === 'fb') {
+        $facebookId = $this->params->id;
+        if ($user->checkFacebook($facebookId)) {
             $response = array(
                 'status' => 'success',
                 'data' => $user,
             );
-            $this->response(200, $response);
+            return   $this->response(200, $response);
         } else {
-            $this->response(401, array(
-                'status' => 'False',
-                'message' => 'lỗi'
-            ));
+            $data = json_decode(file_get_contents("php://input"));
+            $user->full_name = isset($data->name) ? $data->name : '';
+            $user->email = isset($data->email) ? $data->email : '';
+            $user->phone = isset($data->phone) ? $data->phone : '';
+            $user->password = md5($facebookId);
+            $user->status = 0;
+            $user->administration = 0;
+            $user->img_user = isset($data->picture) ? $data->picture->data->url : '';
+            if ($user->create(null, $facebookId)) {
+                $response = array(
+                    'status' => 'success',
+                    'data' => $user,
+                );
+                return    $this->response(200, $response);
+            } else {
+                $this->response(401, array(
+                    'status' => 'False',
+                    'message' => 'lỗi'
+                ));
+            }
         }
-    }
-} elseif ($type === 'gg') {
-    $googleId = $this->params->googleId;
-    if ($user->checkGoogle($googleId)) {
+    } elseif ($type === 'gg') {
+        $googleId = $this->params->googleId;
+        if ($user->checkGoogle($googleId)) {
 
-        $response = array(
-            'status' => 'success',
-            'data' => $user,
-        );
-        $this->response(200, $response);
-    } else {
-        $data = json_decode(file_get_contents("php://input"));
-        $user->full_name = isset($data->name) ? $data->name : '';
-        $user->email = isset($data->email) ? $data->email : '';
-        $user->phone = isset($data->phone) ? $data->phone : '';
-        $user->password = md5($googleId);
-        $user->status = 0;
-        $user->administration = 0;
-        $user->img_user = isset($data->imageUrl) ? $data->imageUrl : '';
-        if ($user->create($googleId)) {
             $response = array(
                 'status' => 'success',
                 'data' => $user,
             );
-            $this->response(200, $response);
+            return $this->response(200, $response);
         } else {
-            $this->response(401, array(
-                'status' => 'False',
-                'message' => 'lỗi'
-            ));
+            $data = json_decode(file_get_contents("php://input"));
+            $user->full_name = isset($data->name) ? $data->name : '';
+            $user->email = isset($data->email) ? $data->email : '';
+            $user->phone = isset($data->phone) ? $data->phone : '';
+            $user->password = md5($googleId);
+            $user->status = 0;
+            $user->administration = 0;
+            $user->img_user = isset($data->imageUrl) ? $data->imageUrl : '';
+            if ($user->create($googleId)) {
+                $response = array(
+                    'status' => 'success',
+                    'data' => $user,
+                );
+                return $this->response(200, $response);
+            } else {
+                $this->response(401, array(
+                    'status' => 'False',
+                    'message' => 'lỗi'
+                ));
+                $this->response(200, $response);
+            }
         }
     }
 } else {
