ca0ab64b3ae43349f6b773d69fe8b08bd9be5ab6
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-12-03 12:47:51 +0700

    fix login

--- controllers/auth/login.php
+++ controllers/auth/login.php
@@ -72,7 +72,6 @@ if (isset($type)) {
 } else {
     $username = $this->params->username;
     $password =  md5($this->params->password);
-    print_r($username . '-' . $password);
     if ($user->login($username, $password)) {
 
         $response = array(
