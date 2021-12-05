33f2ce323bd6fdd3656040bd1125a83f985f78d3
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-12-05 09:12:45 +0700

    fix module

--- module/phpqrcode/qrlib.php
+++ module/phpqrcode/qrlib.php
@@ -25,18 +25,18 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-$QR_BASEDIR = '.' . DIRECTORY_SEPARATOR;
+$QR_BASEDIR = "./";
 
 // Required libs
 
-include $QR_BASEDIR . "qrconst.php";
-include $QR_BASEDIR . "qrconfig.php";
-include $QR_BASEDIR . "qrtools.php";
-include $QR_BASEDIR . "qrspec.php";
-include $QR_BASEDIR . "qrimage.php";
-include $QR_BASEDIR . "qrinput.php";
-include $QR_BASEDIR . "qrbitstream.php";
-include $QR_BASEDIR . "qrsplit.php";
-include $QR_BASEDIR . "qrrscode.php";
-include $QR_BASEDIR . "qrmask.php";
-include $QR_BASEDIR . "qrencode.php";
+include("qrconst.php");
+include("qrconfig.php");
+include("qrtools.php");
+include("qrspec.php");
+include("qrimage.php");
+include("qrinput.php");
+include("qrbitstream.php");
+include("qrsplit.php");
+include("qrrscode.php");
+include("qrmask.php");
+include("qrencode.php");
